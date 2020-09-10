26293c918a431727804483408fefc80c8af58e36
sivadurga mamidala <sivadurgamamidala@gmail.com>
2020-09-10 08:39:35 +0530

    concept on event binding

--- my-second-app/src/app/servers/servers.component.ts
+++ my-second-app/src/app/servers/servers.component.ts
@@ -4,18 +4,25 @@ import { Component, OnInit } from '@angular/core';
   selector: 'app-servers',
   // selector: '[app-servers]',
   // selector: '.app-servers',
-  // templateUrl: './servers.component.html',
+  templateUrl: './servers.component.html',
   // template: '<app-server></app-server><app-server></app-server>',
-  template: `
-  <app-server></app-server>
-  <app-server></app-server>
-  `,
+  // template: `
+  // <app-server></app-server>
+  // <app-server></app-server>
+  // `,
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
-
-  constructor() { }
-
+  // Property binding
+  allowNewServer = false;
+  constructor() { 
+    setTimeout(() => {this.allowNewServer = true} ,3000);
+  }
+  //Event binding
+  serverCreationStatus = 'No Server was created!';
+  onCreationServer(){
+    this.serverCreationStatus = 'Server was created!';
+  }
   ngOnInit(): void {
   }
 
