81b77de7795d4ae8b9e36132314a5317530946d8
sivadurga mamidala <sivadurgamamidala@gmail.com>
2020-11-09 12:08:18 +0530

    Added @viewchild and Getting assess to template & DOM with @viewchild

--- my-third-app/src/app/cockpit/cockpit.component.ts
+++ my-third-app/src/app/cockpit/cockpit.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { Component, ElementRef, EventEmitter, OnInit, Output, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-cockpit',
@@ -9,8 +9,8 @@ export class CockpitComponent implements OnInit {
   @Output() serverCreated = new EventEmitter<{serverName: string, serverContent:string}>();
   @Output('bpCreated') blueprintCreated = new EventEmitter<{serverName: string, serverContent:string}>();
   // newServerName = '';
-  newServerContent = '';
-  
+  // newServerContent = '';
+  @ViewChild('serverContentInput') serverContentInput:ElementRef;
   constructor() { }
 
   // onAddServer(){
@@ -21,14 +21,23 @@ export class CockpitComponent implements OnInit {
   //   // });
   //   this.serverCreated.emit({serverName: this.newServerName, serverContent: this.newServerContent});
   // }
+  // onAddServer(nameInput: HTMLInputElement){
+  //   // this.serverElements.push({
+  //   //   type: 'server',
+  //   //   name: this.newServerName,
+  //   //   content: this.newServerContent
+  //   // });
+  //   this.serverCreated.emit({serverName: nameInput.value, serverContent: this.newServerContent});
+  // }
   onAddServer(nameInput: HTMLInputElement){
     // this.serverElements.push({
     //   type: 'server',
     //   name: this.newServerName,
     //   content: this.newServerContent
     // });
-    this.serverCreated.emit({serverName: nameInput.value, serverContent: this.newServerContent});
+    this.serverCreated.emit({serverName: nameInput.value, serverContent: this.serverContentInput.nativeElement.value});
   }
+
   
   // onAddBluePrint(){
   //   // this.serverElements.push({
@@ -38,13 +47,21 @@ export class CockpitComponent implements OnInit {
   //   // });
   //   this.blueprintCreated.emit({serverName: this.newServerName, serverContent: this.newServerContent});
   // }
+  // onAddBluePrint(nameInput: HTMLInputElement){
+  //   // this.serverElements.push({
+  //   //   type: 'blueprint',
+  //   //   name: this.newServerName,
+  //   //   content: this.newServerContent
+  //   // });
+  //   this.blueprintCreated.emit({serverName: nameInput.value, serverContent: this.newServerContent});
+  // }
   onAddBluePrint(nameInput: HTMLInputElement){
     // this.serverElements.push({
     //   type: 'blueprint',
     //   name: this.newServerName,
     //   content: this.newServerContent
     // });
-    this.blueprintCreated.emit({serverName: nameInput.value, serverContent: this.newServerContent});
+    this.blueprintCreated.emit({serverName: nameInput.value, serverContent: this.serverContentInput.nativeElement.value});
   }
   ngOnInit(): void {
 
