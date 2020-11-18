9e08fa1f1d3bfefe21fb90b292ba08cd98a3a280
sivadurga mamidala <sivadurgamamidala@gmail.com>
2020-11-19 04:19:42 +0530

    Passing data with Event and property Binding(combined)

--- myproject/src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
+++ myproject/src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
@@ -1,4 +1,4 @@
-import { Component,  Input, OnInit } from '@angular/core';
+import { Component,  EventEmitter,  Input, OnInit, Output } from '@angular/core';
 import { Recipe } from '../../recipe.model';
 
 @Component({
@@ -8,13 +8,12 @@ import { Recipe } from '../../recipe.model';
 })
 export class RecipeItemComponent implements OnInit {
   @Input() recipe: Recipe;
+  @Output() recipeSlected = new EventEmitter<void>();
   constructor() { }
 
   ngOnInit(): void {
   }
-  // recipes: Recipe[] = [
-  //   new Recipe('A test recipe 1', 'This is a simple Recipe 1', 'https://c.ndtvimg.com/2019-05/ebsmi4t8_indian-curry_625x300_14_May_19.jpg'),
-  //   new Recipe('A test recipe 2', 'This is a simple Recipe 2', 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRbdZcWBIc1UYdO7wmsCJIKd5eGX9EF6eEyXg&usqp=CAU')
-  // ];
-
+  onSelected(){
+    this.recipeSlected.emit();
+  }
 }
--- myproject/src/app/recipes/recipe-list/recipe-list.component.ts
+++ myproject/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 
 import{ Recipe } from '../recipe.model';
 
@@ -8,6 +8,7 @@ import{ Recipe } from '../recipe.model';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
   recipes: Recipe[] = [
     new Recipe('A test recipe 1', 'This is a simple Recipe 1', 'https://c.ndtvimg.com/2019-05/ebsmi4t8_indian-curry_625x300_14_May_19.jpg'),
     new Recipe('A test recipe 2', 'This is a simple Recipe 2', 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRbdZcWBIc1UYdO7wmsCJIKd5eGX9EF6eEyXg&usqp=CAU')
@@ -16,5 +17,7 @@ export class RecipeListComponent implements OnInit {
 
   ngOnInit(): void {
   }
-
+  onRecipeSelected(recipe: Recipe){
+    this.recipeWasSelected.emit(recipe);
+  }
 }
