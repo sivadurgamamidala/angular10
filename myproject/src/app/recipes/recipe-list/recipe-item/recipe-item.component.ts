bf410cf9cf4f7c5b59415991029ad2b4a03ef135
sivadurga mamidala <sivadurgamamidala@gmail.com>
2020-11-18 13:23:05 +0530

    Passing Recipe Data with Proprety Binding

--- myproject/src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
+++ myproject/src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component,  Input, OnInit } from '@angular/core';
+import { Recipe } from '../../recipe.model';
 
 @Component({
   selector: 'app-recipe-item',
@@ -6,10 +7,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-item.component.css']
 })
 export class RecipeItemComponent implements OnInit {
-  
+  @Input() recipe: Recipe;
   constructor() { }
 
   ngOnInit(): void {
   }
+  // recipes: Recipe[] = [
+  //   new Recipe('A test recipe 1', 'This is a simple Recipe 1', 'https://c.ndtvimg.com/2019-05/ebsmi4t8_indian-curry_625x300_14_May_19.jpg'),
+  //   new Recipe('A test recipe 2', 'This is a simple Recipe 2', 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRbdZcWBIc1UYdO7wmsCJIKd5eGX9EF6eEyXg&usqp=CAU')
+  // ];
 
 }
