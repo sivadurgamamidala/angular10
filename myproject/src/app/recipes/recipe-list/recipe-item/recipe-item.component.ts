5d8c926ae7237836eb3261771be2a8ba99a7bddc
sivadurga mamidala <sivadurgamamidala@gmail.com>
2020-12-01 01:09:17 +0530

    Using a service for cross-component communication

--- myproject/src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
+++ myproject/src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
@@ -1,5 +1,6 @@
 import { Component,  EventEmitter,  Input, OnInit, Output } from '@angular/core';
 import { Recipe } from '../../recipe.model';
+import { RecipeService } from '../../recipe.service';
 
 @Component({
   selector: 'app-recipe-item',
@@ -8,12 +9,13 @@ import { Recipe } from '../../recipe.model';
 })
 export class RecipeItemComponent implements OnInit {
   @Input() recipe: Recipe;
-  @Output() recipeSlected = new EventEmitter<void>();
-  constructor() { }
+  // @Output() recipeSlected = new EventEmitter<void>();
+  constructor(private recipeService:RecipeService) { }
 
   ngOnInit(): void {
   }
   onSelected(){
-    this.recipeSlected.emit();
+    // this.recipeSlected.emit();
+    this.recipeService.recipeSelected.emit(this.recipe);
   }
 }
--- myproject/src/app/recipes/recipe-list/recipe-list.component.ts
+++ myproject/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,6 @@
-import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+// import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
+
 
 import{ Recipe } from '../recipe.model';
 import { RecipeService } from "../recipe.service";
@@ -9,7 +11,7 @@ import { RecipeService } from "../recipe.service";
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-  @Output() recipeWasSelected = new EventEmitter<Recipe>();
+  // @Output() recipeWasSelected = new EventEmitter<Recipe>();
   // recipes: Recipe[] = [
   //   new Recipe('A test recipe 1', 'This is a simple Recipe 1', 'https://c.ndtvimg.com/2019-05/ebsmi4t8_indian-curry_625x300_14_May_19.jpg'),
   //   new Recipe('A test recipe 2', 'This is a simple Recipe 2', 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRbdZcWBIc1UYdO7wmsCJIKd5eGX9EF6eEyXg&usqp=CAU')
@@ -20,7 +22,7 @@ export class RecipeListComponent implements OnInit {
   ngOnInit(): void {
     this.recipes = this.recipeService.getRecipes();
   }
-  onRecipeSelected(recipe: Recipe){
-    this.recipeWasSelected.emit(recipe);
-  }
+  // onRecipeSelected(recipe: Recipe){
+  //   this.recipeWasSelected.emit(recipe);
+  // }
 }
