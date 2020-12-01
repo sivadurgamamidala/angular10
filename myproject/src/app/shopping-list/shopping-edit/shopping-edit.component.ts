e38023788719f50a56df8bcedb2322971a5e58f2
sivadurga mamidala <sivadurgamamidala@gmail.com>
2020-12-02 01:01:41 +0530

    upto date

--- myproject/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ myproject/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,5 +1,8 @@
-import { Component, ElementRef, EventEmitter, OnInit, Output, ViewChild } from '@angular/core';
+// import { Component, ElementRef, EventEmitter, OnInit, Output, ViewChild } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+
 import{ Ingredient } from "../../shared/ingredient.model";
+import { ShoppingListService } from '../shopping-list.service';
 
 @Component({
   selector: 'app-shopping-edit',
@@ -10,9 +13,9 @@ export class ShoppingEditComponent implements OnInit {
   @ViewChild('nameInput') nameInputRef: ElementRef;
   @ViewChild('amountInput') amountInputRef: ElementRef;
   // IngredientAdded = new EventEmitter<{name: string,amount: number}>();
-  @Output() ingredientAdded = new EventEmitter<{name: string,amount: number}>();
+  // @Output() ingredientAdded = new EventEmitter<{name: string,amount: number}>();
 
-  constructor() { }
+  constructor(private shoppingListService:ShoppingListService) { }
 
   ngOnInit(): void {
   }
@@ -20,7 +23,8 @@ export class ShoppingEditComponent implements OnInit {
     const ingName = this.nameInputRef.nativeElement.value;
     const ingAmount = this.amountInputRef.nativeElement.value;
     const newIngredient = new Ingredient(ingName,ingAmount);
-    this.ingredientAdded.emit(newIngredient);
+    // this.ingredientAdded.emit(newIngredient);
+    this.shoppingListService.addIngredients(newIngredient);
   }
   onDeleteItem(){
 
