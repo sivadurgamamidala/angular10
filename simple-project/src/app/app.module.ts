346c462ca9a84692228d06db4b5bffc50eb8d2d0
sivadurga mamidala <sivadurgamamidala@gmail.com>
2020-12-05 01:53:50 +0530

    simple project

---
+++ simple-project/src/app/app.module.ts
@@ -0,0 +1,36 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { RouterModule, Routes } from '@angular/router';
+
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { UsersComponent } from './users/users.component';
+import { ServersComponent } from './servers/servers.component';
+
+const appRoutes:Routes = [
+  {path: "", component:HomeComponent},
+  {path:"users", component:UsersComponent},
+  {path:"servers", component:ServersComponent}
+];
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    UsersComponent,
+    ServersComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    AppRoutingModule,
+    RouterModule.forRoot(appRoutes)
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
