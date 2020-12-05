a1d01da31d0aa96069a4a02b518fbf524c29e53c
sivadurga mamidala <sivadurgamamidala@gmail.com>
2020-12-06 02:07:14 +0530

    added files

--- simple-project/src/app/servers/servers.service.ts
+++ simple-project/src/app/servers/servers.service.ts
@@ -27,4 +27,15 @@ export class ServersService{
         );
         return server;
     }
+    updateServer(id:number, serverInfo:{name:string, status:string}){
+        const server = this.servers.find(
+            (s) => {
+                return s.id === id;
+            }
+        );
+        if(server){
+            server.name = serverInfo.name;
+            server.status = serverInfo.status;
+        }
+    }
 }
