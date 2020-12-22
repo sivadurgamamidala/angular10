ca44ecadc63e65bbe1b5eca3c39a03c1a5845f49
sivadurga mamidala <sivadurgamamidala@gmail.com>
2020-12-22 11:50:52 +0530

    redirect and wildcard route

--- routing-start/src/app/app.module.ts
+++ routing-start/src/app/app.module.ts
@@ -1,5 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { RouterModule, Routes } from '@angular/router';
 
@@ -11,7 +11,7 @@ import { UserComponent } from './users/user/user.component';
 import { EditServerComponent } from './servers/edit-server/edit-server.component';
 import { ServerComponent } from './servers/server/server.component';
 import { ServersService } from './servers/servers.service';
-
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 const appRoutes: Routes = [
   {path : "" , component : HomeComponent},
@@ -25,6 +25,8 @@ const appRoutes: Routes = [
   },
   // {path : 'servers/:id', component: ServerComponent},
   // {path : 'servers/:id/edit', component: EditServerComponent}
+  {path : 'not-found', component: PageNotFoundComponent},
+  {path: '**', redirectTo:'/not-found'}
 ];
 
 @NgModule({
@@ -35,7 +37,8 @@ const appRoutes: Routes = [
     ServersComponent,
     UserComponent,
     EditServerComponent,
-    ServerComponent
+    ServerComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
