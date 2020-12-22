cb1c2c3ff28150d676eea607b2a15e776ed1fc41
sivadurga mamidala <sivadurgamamidala@gmail.com>
2020-12-22 13:01:59 +0530

    Outsourcing the Route Configuration

--- routing-start/src/app/app.module.ts
+++ routing-start/src/app/app.module.ts
@@ -12,23 +12,24 @@ import { EditServerComponent } from './servers/edit-server/edit-server.component
 import { ServerComponent } from './servers/server/server.component';
 import { ServersService } from './servers/servers.service';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { AppRoutingModule } from './app.routing.module';
 
-const appRoutes: Routes = [
-  {path : "" , component : HomeComponent},
-  {path : 'users' , component: UsersComponent, children:[
-    {path : ':id/:name' , component: UserComponent}]
-  },
-  // {path : 'users/:id/:name' , component: UsersComponent},
-  {path : 'servers', component: ServersComponent, children: [
-    {path : ':id', component: ServerComponent},
-    {path : ':id/edit', component: EditServerComponent}]
-  },
-  // {path : 'servers/:id', component: ServerComponent},
-  // {path : 'servers/:id/edit', component: EditServerComponent}
-  {path : 'not-found', component: PageNotFoundComponent},
-  // {path: '', redirectTo:'/not-found', pathMatch:'full'},
-  {path: '**', redirectTo:'/not-found'}
-];
+// const appRoutes: Routes = [
+//   {path : "" , component : HomeComponent},
+//   {path : 'users' , component: UsersComponent, children:[
+//     {path : ':id/:name' , component: UserComponent}]
+//   },
+//   // {path : 'users/:id/:name' , component: UsersComponent},
+//   {path : 'servers', component: ServersComponent, children: [
+//     {path : ':id', component: ServerComponent},
+//     {path : ':id/edit', component: EditServerComponent}]
+//   },
+//   // {path : 'servers/:id', component: ServerComponent},
+//   // {path : 'servers/:id/edit', component: EditServerComponent}
+//   {path : 'not-found', component: PageNotFoundComponent},
+//   // {path: '', redirectTo:'/not-found', pathMatch:'full'},
+//   {path: '**', redirectTo:'/not-found'}
+// ];
 
 @NgModule({
   declarations: [
@@ -44,7 +45,8 @@ const appRoutes: Routes = [
   imports: [
     BrowserModule,
     FormsModule,
-    RouterModule.forRoot(appRoutes)
+    AppRoutingModule,
+    // RouterModule.forRoot(appRoutes)
   ],
   providers: [ServersService],
   bootstrap: [AppComponent]
