9fcf06f3cfd57743d6808e1d1d2ad5cb8107b5d9
sivadurga mamidala <sivadurgamamidala@gmail.com>
2021-01-19 20:00:15 +0530

    understanding operators

--- obs-01-start/src/app/home/home.component.ts
+++ obs-01-start/src/app/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
-import { interval, Observable, Subscription } from 'rxjs'
+import { interval, Observable, Subscription } from 'rxjs';
+import { map, filter } from 'rxjs/operators'
 
 @Component({
   selector: 'app-home',
@@ -18,21 +19,25 @@ export class HomeComponent implements OnInit, OnDestroy {
     let count = 0;
     setInterval(() => {
       observer.next(count);
-      if(count === 4){
+      if(count === 5){
         observer.complete();
       }
-      if(count>2){
+      if(count > 3){
         //this will not executed the completed statement or console.log("completed!") . at the end only shows an error message 
-          observer.error(new Error("count is greater than 2!"))
+          observer.error(new Error("count is greater than 3!"))
         }
       // if(count>5){
       //   observer.error(new Error("count is greater than 5!"))
       // }
       count++;
     },1000);
-    
+
    });
-   this.firstObsSubscription = customIntervalObservable.subscribe(data => {
+
+   
+   this.firstObsSubscription = customIntervalObservable.pipe(filter(data => { return data > 0 }),map((data:number) => {
+    return 'Round: ' + (data+1);
+  })).subscribe(data => {
      console.log(data)
    }, error => {
      console.log(error);
