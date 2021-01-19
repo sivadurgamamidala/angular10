7544d6c72a4d7f354f82edfdc1f687a57be55d86
sivadurga mamidala <sivadurgamamidala@gmail.com>
2021-01-19 19:13:37 +0530

    Errors and completion difference

--- obs-01-start/src/app/home/home.component.ts
+++ obs-01-start/src/app/home/home.component.ts
@@ -18,13 +18,29 @@ export class HomeComponent implements OnInit, OnDestroy {
     let count = 0;
     setInterval(() => {
       observer.next(count);
+      if(count === 4){
+        observer.complete();
+      }
+      if(count>2){
+        //this will not executed the completed statement or console.log("completed!") . at the end only shows an error message 
+          observer.error(new Error("count is greater than 2!"))
+        }
+      // if(count>5){
+      //   observer.error(new Error("count is greater than 5!"))
+      // }
       count++;
     },1000);
     
    });
    this.firstObsSubscription = customIntervalObservable.subscribe(data => {
      console.log(data)
-   });
+   }, error => {
+     console.log(error);
+     alert(error.message);
+   }, () => {
+     console.log("Completed!")
+   }
+   );
   }
 
   ngOnDestroy(){
