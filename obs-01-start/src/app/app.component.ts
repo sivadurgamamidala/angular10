975b82813697a4999853ade170eaa65832f0a60e
sivadurga mamidala <sivadurgamamidala@gmail.com>
2021-01-20 11:49:21 +0530

    this is the old approach with the event emitter and make button activated

--- obs-01-start/src/app/app.component.ts
+++ obs-01-start/src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { UserService } from './user.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'obs-O1-start';
+export class AppComponent implements OnInit {
+  userActivated = false;
+
+  constructor(private userService:UserService){}
+  
+  ngOnInit(){
+    this.userService.activatedEmitter.subscribe(didActivate => {
+      this.userActivated = didActivate;
+    })
+  }
 }
