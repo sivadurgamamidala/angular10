cf4df3ad788a50ac7f83a27d0d7dd38e0cf5abbb
sivadurga mamidala <sivadurgamamidala@gmail.com>
2021-01-20 18:55:35 +0530

    Submitting and using the form

--- forms-td-start/src/app/app.component.ts
+++ forms-td-start/src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -8,5 +9,12 @@ import { Component } from '@angular/core';
 export class AppComponent {
   suggestUserName() {
     const suggestedName = 'Superuser';
+  }
+  // onSubmit(form: ElementRef){
+  //   // console.log("Submitted!")
+  //   console.log(form);
+  // }
+  onSubmit(form: NgForm){
+    console.log(form);
   }
 }
