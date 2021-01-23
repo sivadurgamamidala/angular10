31893fbc527f7fe98e2ea1bea8d058ec54e2ce4c
sivadurga mamidala <sivadurgamamidala@gmail.com>
2021-01-23 15:34:09 +0530

    Using form data: (display the details of user by using form data)

--- forms-td-start/src/app/app.component.ts
+++ forms-td-start/src/app/app.component.ts
@@ -12,6 +12,14 @@ export class AppComponent {
   answer = "";
   genders = ['Male', 'Female'];
   defaultGender = 'Male';
+  user = {
+    username: "",
+    email:"",
+    secretQuestion:"",
+    answer:"",
+    gender:""
+  }
+  submitted = false;
 
   suggestUserName() {
     const suggestedName = 'Superuser';
@@ -39,6 +47,12 @@ export class AppComponent {
   //   console.log(form);
   // }
   onSubmit(){
-    console.log(this.signupForm);
+    // console.log(this.signupForm);
+    this.submitted = true;
+    this.user.username = this.signupForm.value.userData.username;
+    this.user.email = this.signupForm.value.userData.email;
+    this.user.secretQuestion = this.signupForm.value.secret;
+    this.user.answer = this.signupForm.value.questionAnswer;
+    this.user.gender = this.signupForm.value.gender; 
   }
 }
