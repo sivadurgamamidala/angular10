848326c15ac073d71f720bc197cf6c6955d47c20
sivadurga mamidala <sivadurgamamidala@gmail.com>
2021-01-24 16:10:57 +0530

    reactive: Using error codes (the key thing is to understand that these error codes can be used to show the right error messages)

--- forms-reactive-start/src/app/app.component.ts
+++ forms-reactive-start/src/app/app.component.ts
@@ -9,6 +9,7 @@ import { FormArray, FormControl, FormGroup, Validators } from '@angular/forms';
 export class AppComponent implements OnInit{
   genders = ['male', 'female'];
   signupForm: FormGroup;
+  forbiddenUsernames = ['ramesh','venkatesh'];
 
   // ngOnInit(){
   //   this.signupForm = new FormGroup({
@@ -19,7 +20,7 @@ export class AppComponent implements OnInit{
   ngOnInit(){
     this.signupForm = new FormGroup({
       'userData' : new FormGroup({
-        'username' : new FormControl(null, Validators.required),
+        'username' : new FormControl(null, [Validators.required, this.forbiddenNames.bind(this)]),
         'email' : new FormControl(null, [Validators.required,Validators.email])
       }),
       'gender' : new FormControl('male'),
@@ -32,5 +33,12 @@ export class AppComponent implements OnInit{
   onAddHobby(){
     const control = new FormControl(null, Validators.required);
     (<FormArray>this.signupForm.get('hobbies')).push(control);
+  }
+
+  forbiddenNames(control: FormControl) : {[s:string] : boolean } {
+    if(this.forbiddenUsernames.indexOf(control.value) !== -1) {
+      return {'nameIsForbidden': true};
+    }
+    return null;
   }
 }
