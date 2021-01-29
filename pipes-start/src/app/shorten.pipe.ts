92a4be609ce45684e58570b0199fd37e189618df
sivadurga mamidala <sivadurgamamidala@gmail.com>
2021-01-29 22:35:54 +0530

    nothing changed

--- pipes-start/src/app/shorten.pipe.ts
+++ pipes-start/src/app/shorten.pipe.ts
@@ -7,9 +7,8 @@ export class ShortenPipe implements PipeTransform {
     transform(value : any) {
         if(value.length > 10) {
             return (value.substr(0,10) + ' ...');
-        } else {
-            return value;
-        }
+        } 
+        return value;
         
     }
 }
