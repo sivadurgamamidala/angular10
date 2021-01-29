9dcac61348787527586d184da9e2d31e82ce337a
sivadurga mamidala <sivadurgamamidala@gmail.com>
2021-01-29 17:36:00 +0530

    some finishing touches (removing the memory leaks)

--- myproject/src/app/recipes/recipe-list/recipe-list.component.ts
+++ myproject/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,6 +1,7 @@
 // import { Component, EventEmitter, OnInit, Output } from '@angular/core';
-import { Component, OnInit} from '@angular/core';
+import { Component, OnDestroy, OnInit} from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
+import { Subscription } from 'rxjs';
 
 
 import{ Recipe } from '../recipe.model';
@@ -11,25 +12,32 @@ import { RecipeService } from "../recipe.service";
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
-export class RecipeListComponent implements OnInit {
+export class RecipeListComponent implements OnInit, OnDestroy {
   // @Output() recipeWasSelected = new EventEmitter<Recipe>();
   // recipes: Recipe[] = [
   //   new Recipe('A test recipe 1', 'This is a simple Recipe 1', 'https://c.ndtvimg.com/2019-05/ebsmi4t8_indian-curry_625x300_14_May_19.jpg'),
   //   new Recipe('A test recipe 2', 'This is a simple Recipe 2', 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRbdZcWBIc1UYdO7wmsCJIKd5eGX9EF6eEyXg&usqp=CAU')
   // ];
   recipes: Recipe[];
+  subcription : Subscription;
+
   constructor(private recipeService:RecipeService,
               private router : Router,
               private route : ActivatedRoute) { 
   }
 
   ngOnInit(): void {
-    this.recipeService.recipesChanged
+    this.subcription = this.recipeService.recipesChanged
       .subscribe((recipes:Recipe[]) => {
         this.recipes = recipes;
       });
     this.recipes = this.recipeService.getRecipes();
   }
+
+  ngOnDestroy() {
+    this.subcription.unsubscribe();
+  }
+
   onNewRecipe(){
     this.router.navigate(['new'], {relativeTo: this.route});
   }
