9cb85ae16e696d95962c401966190dbf56dff73b
sivadurga mamidala <sivadurgamamidala@gmail.com>
2021-02-01 12:25:30 +0530

    GETting Data

--- http-01-start/src/app/app.component.ts
+++ http-01-start/src/app/app.component.ts
@@ -11,7 +11,9 @@ export class AppComponent implements OnInit {
 
   constructor(private http: HttpClient) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.fetchPosts();
+  }
 
   onCreatePost(postData: { title: string; content: string }) {
     // Send Http request
@@ -26,9 +28,16 @@ export class AppComponent implements OnInit {
 
   onFetchPosts() {
     // Send Http request
+    this.fetchPosts();
   }
 
   onClearPosts() {
     // Send Http request
+  }
+
+  private fetchPosts() {
+    this.http.get('https://http-01start-default-rtdb.firebaseio.com/posts.json').subscribe(posts => {
+      console.log(posts);
+    });
   }
 }
