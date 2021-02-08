13536c08a021b0d881a701f2c6b56a832e24f445
sivadurga mamidala <sivadurgamamidala@gmail.com>
2021-02-08 10:14:50 +0530

    outputting posts and showing a loading indicator

--- http-01-start/src/app/app.component.ts
+++ http-01-start/src/app/app.component.ts
@@ -10,7 +10,8 @@ import { Post } from './post.model';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
-  loadedPosts = [];
+  loadedPosts:Post[] = [];
+  isFetching = false;
 
   constructor(private http: HttpClient) {}
 
@@ -39,6 +40,7 @@ export class AppComponent implements OnInit {
   }
 
   private fetchPosts() {
+    this.isFetching = true;
     this.http
     .get<{[key: string]: Post}>('https://http-01start-default-rtdb.firebaseio.com/posts.json')
     .pipe(map(responseData => {
@@ -51,7 +53,9 @@ export class AppComponent implements OnInit {
       return postsArray;
     }))
     .subscribe(posts => {
-      console.log(posts);
+      // console.log(posts);
+      this.isFetching = false;
+      this.loadedPosts = posts;
     });
   }
 }
