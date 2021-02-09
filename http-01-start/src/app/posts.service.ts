f5d14960d690f109486d9044d7ce00509bda1164
sivadurga mamidala <sivadurgamamidala@gmail.com>
2021-02-09 20:14:56 +0530

    using the catchError operator

--- http-01-start/src/app/posts.service.ts
+++ http-01-start/src/app/posts.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
-import { Subject } from "rxjs";
-import { map } from "rxjs/operators";
+import { Subject, throwError } from "rxjs";
+import { map, catchError } from "rxjs/operators";
 
 import { Post } from "./post.model";
 
@@ -38,7 +38,11 @@ export class PostsService {
                     }
                     return postsArray;
                 }
-            )
+            ),
+            catchError(errorRes => {
+                //send to analytics server
+                return throwError(errorRes);
+            })
         );
     }
     deletePosts() {
